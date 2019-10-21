b438fd1b96654ed9423982d283a9f043a3daa6b2
FahimFerdous <fahimferdous119@gmail.com>
2019-10-21 22:39:07 +0600

    Haldabi Issue Solved

--- src/app/model/sudCalculation.ts
+++ src/app/model/sudCalculation.ts
@@ -1,54 +1,50 @@
-import { UserInfos } from './user-inofo';
+import { UserInfos } from "./user-inofo";
 
 export class SudCalculation {
+  SudCalculationActionHandaler(userInfos: UserInfos) {
+    userInfos.halDabi = parseFloat(userInfos.jomirPoriman) * 25;
+    var currentDateObj = new Date();
+    var currentyearInEnglish = currentDateObj.getUTCFullYear();
 
-     SudCalculationActionHandaler(userInfos:UserInfos){
-   
-         userInfos.halDabi = parseInt(userInfos.jomirPoriman) * 25;
-        var currentDateObj = new Date();
-        var currentyearInEnglish = currentDateObj.getUTCFullYear();
-    
-            var currentYearInBangla = currentyearInEnglish - 593;
-        userInfos.bokeyaBosor =
-            currentYearInBangla - parseInt(userInfos.sorbosesKhajnaPorisodherBosor)-1;
+    var currentYearInBangla = currentyearInEnglish - 593;
+    userInfos.bokeyaBosor =
+      currentYearInBangla -
+      parseInt(userInfos.sorbosesKhajnaPorisodherBosor) -
+      1;
 
+    if (userInfos.bokeyaBosor > 1) {
+      let calculateYear = userInfos.bokeyaBosor * (userInfos.bokeyaBosor + 1);
+      let DivisonResult = calculateYear / 32;
 
-            if (userInfos.bokeyaBosor > 1) {
-        
-                let calculateYear = userInfos.bokeyaBosor * (userInfos.bokeyaBosor + 1);
-                let DivisonResult = calculateYear / 32;
-        
-                let bokeyaDabirSud = DivisonResult * userInfos.halDabi;
-        
-                let tempMotDabi = (userInfos.bokeyaBosor + 1) * userInfos.halDabi;
-        
-                let MotDabi = bokeyaDabirSud + tempMotDabi;
-        
-                userInfos.motDabi = MotDabi;
-        
-                userInfos.bokeyaDabirSud = bokeyaDabirSud;
+      let bokeyaDabirSud = DivisonResult * userInfos.halDabi;
 
-              } else {
-                if (userInfos.bokeyaBosor<=0) {
-                    userInfos.bokeyaBosor = 1;
-                }
-                let step1 = userInfos.bokeyaBosor;
-        
-                let tempsud = Math.round(userInfos.bokeyaDabi * 0.0625);
-        
-                let bokeyaDabirSud = tempsud * step1;
-        
-                let motDabi =
-                  bokeyaDabirSud +
-                  parseInt(userInfos.bokeyaDabi) +
-                  parseInt(userInfos.halDabi);
-                  userInfos.motDabi = motDabi;
-        
-                  userInfos.bokeyaDabirSud = bokeyaDabirSud;
-                  userInfos.bokeyaBosor = 0;
-              }
-        
-            
-         return userInfos;
-     }
- }
+      let tempMotDabi = (userInfos.bokeyaBosor + 1) * userInfos.halDabi;
+
+      let MotDabi = bokeyaDabirSud + tempMotDabi;
+
+      userInfos.motDabi = MotDabi;
+
+      userInfos.bokeyaDabirSud = bokeyaDabirSud;
+    } else {
+      if (userInfos.bokeyaBosor <= 0) {
+        userInfos.bokeyaBosor = 1;
+      }
+      let step1 = userInfos.bokeyaBosor;
+
+      let tempsud = Math.round(userInfos.bokeyaDabi * 0.0625);
+
+      let bokeyaDabirSud = tempsud * step1;
+
+      let motDabi =
+        bokeyaDabirSud +
+        parseInt(userInfos.bokeyaDabi) +
+        parseInt(userInfos.halDabi);
+      userInfos.motDabi = motDabi;
+
+      userInfos.bokeyaDabirSud = bokeyaDabirSud;
+      userInfos.bokeyaBosor = 0;
+    }
+
+    return userInfos;
+  }
+}
