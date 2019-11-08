1fcd534778af2139548c550527f187bed0371ef4
Rimon19 <mominur.hossain@outlook.com>
2019-11-08 23:55:38 +0600

    groupByAddedInAprovepaymentlistpage

--- src/app/pages/aprovepayment-list/aprovepayment-list.page.ts
+++ src/app/pages/aprovepayment-list/aprovepayment-list.page.ts
@@ -1,3 +1,5 @@
+import { Key } from 'protractor';
+
 import { Component, OnInit } from '@angular/core';
 import { UserInfosService } from "./../../services/user-infos.service";
 import { Subscription } from "rxjs";
@@ -10,9 +12,9 @@ import { UserInfos } from '../../model/user-inofo';
 export class AprovepaymentListPage implements OnInit {
   subscription:Subscription
   userInfos:UserInfos[]=[];
-  constructor( private userInfoService: UserInfosService) { }
-
-  ngOnInit() {
+  userInfosAfter:UserInfos[]=[];
+  
+  constructor( private userInfoService: UserInfosService) { 
 
     var allUserInfo = this.userInfoService.getAllUserInfos();
     this.subscription= allUserInfo.snapshotChanges().pipe().subscribe(item => {
@@ -24,11 +26,42 @@ export class AprovepaymentListPage implements OnInit {
             this.userInfos.push(y as UserInfos);
         }  
       })
-  console.log(this.userInfos);
+      console.log('userInfos',this.userInfos);
+      const grouped =this.groupBy(this.userInfos, pet => pet.userUniCode);
+    
+      grouped.forEach(item=>{
+      this.userInfosAfter.push(item[0]);
 
+      });     
+    //   this.userInfosAfter.forEach(item=>{
+    //     this.userInfoAfterGroup.push(item);
+      console.log('userInfosAfter',this.userInfosAfter);
+    //  });
       });
 
   }
+
+  ngOnInit() {
+
+   
+  }
+
+
+
+
+ groupBy(list, keyGetter) {
+          const map = new Map();
+          list.forEach((item) => {
+              const key = keyGetter(item);
+              const collection = map.get(key);
+              if (!collection) {
+                  map.set(key, [item]);
+              } else {
+                  collection.push(item);
+              }
+          });
+          return map;
+      }
 
   getRowClass(row){
 
