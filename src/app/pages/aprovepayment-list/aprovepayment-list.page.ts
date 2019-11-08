1d4ab083d44a7568a4b1da3152e5e9a6ea852213
Rimon19 <mominur.hossain@outlook.com>
2019-11-08 10:33:22 +0600

    ngxDatatableAddedInApproveMentList

--- src/app/pages/aprovepayment-list/aprovepayment-list.page.ts
+++ src/app/pages/aprovepayment-list/aprovepayment-list.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserInfosService } from "./../../services/user-infos.service";
+import { Subscription } from "rxjs";
+import { UserInfos } from '../../model/user-inofo';
 @Component({
   selector: 'aprovepayment-list',
   templateUrl: './aprovepayment-list.page.html',
   styleUrls: ['./aprovepayment-list.page.scss'],
 })
 export class AprovepaymentListPage implements OnInit {
-
-  constructor() { }
+  subscription:Subscription
+  userInfos:UserInfos[]=[];
+  constructor( private userInfoService: UserInfosService) { }
 
   ngOnInit() {
+
+    var allUserInfo = this.userInfoService.getAllUserInfos();
+    this.subscription= allUserInfo.snapshotChanges().pipe().subscribe(item => {
+      this.userInfos = [];
+      item.forEach(element => {
+        var y = element.payload.toJSON();
+        if(y['approved']==true){
+            y["key"] = element.key;                   
+            this.userInfos.push(y as UserInfos);
+        }  
+      })
+  console.log(this.userInfos);
+
+      });
+
   }
 
+  getRowClass(row){
+
+  }
+  async open(row){
+    
+  }
 }
