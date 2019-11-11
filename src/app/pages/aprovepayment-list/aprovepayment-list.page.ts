046673be4ede4c5403842703b6081c109a2ff334
FahimFerdous <fahimferdous119@gmail.com>
2019-11-11 22:46:40 +0600

    payment Info Design Done

--- src/app/pages/aprovepayment-list/aprovepayment-list.page.ts
+++ src/app/pages/aprovepayment-list/aprovepayment-list.page.ts
@@ -1,86 +1,82 @@
-import { Key } from 'protractor';
+import { Key } from "protractor";
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { UserInfosService } from "./../../services/user-infos.service";
 import { Subscription } from "rxjs";
-import { UserInfos } from '../../model/user-inofo';
-import { Router } from '@angular/router';
+import { UserInfos } from "../../model/user-inofo";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'aprovepayment-list',
-  templateUrl: './aprovepayment-list.page.html',
-  styleUrls: ['./aprovepayment-list.page.scss'],
+  selector: "aprovepayment-list",
+  templateUrl: "./aprovepayment-list.page.html",
+  styleUrls: ["./aprovepayment-list.page.scss"]
 })
 export class AprovepaymentListPage implements OnInit {
-  subscription:Subscription
-  userInfos:UserInfos[]=[];
-  userInfosAfter:UserInfos[]=[];
-  
-  constructor( private userInfoService: UserInfosService,
-    private router:Router) { 
+  subscription: Subscription;
+  userInfos: UserInfos[] = [];
+  userInfosAfter: UserInfos[] = [];
 
+  tableStyle = "bootstrap";
+
+  constructor(
+    private userInfoService: UserInfosService,
+    private router: Router
+  ) {
     var allUserInfo = this.userInfoService.getAllUserInfos();
-    this.subscription= allUserInfo.snapshotChanges().pipe().subscribe(item => {
-      this.userInfos = [];
-      item.forEach(element => {
-        var y = element.payload.toJSON();
-        if(y['approved']==true){
-          console.log(y);
+    this.subscription = allUserInfo
+      .snapshotChanges()
+      .pipe()
+      .subscribe(item => {
+        this.userInfos = [];
+        item.forEach(element => {
+          var y = element.payload.toJSON();
+          if (y["approved"] == true) {
+            console.log(y);
             y["key"] = element.key;
-            
-            if(y['approvedDate']!=undefined){
-              var dateObj = new Date(y['approvedDate']);
+
+            if (y["approvedDate"] != undefined) {
+              var dateObj = new Date(y["approvedDate"]);
               var month = dateObj.getUTCMonth() + 1; //months from 1-12
               var day = dateObj.getUTCDate();
-              var year = dateObj.getUTCFullYear();     
-              y['approvedDate']=day+'/'+month+'/'+year; 
-            } 
-              
+              var year = dateObj.getUTCFullYear();
+              y["approvedDate"] = day + "/" + month + "/" + year;
+            }
+
             this.userInfos.push(y as UserInfos);
-            
-        }  
-      })
-      console.log('userInfos',this.userInfos);
-      // const grouped =this.groupBy(this.userInfos, pet => pet.userUniCode);
-    
-      // grouped.forEach(item=>{
-      // this.userInfosAfter.push(item[0]);
+          }
+        });
+        console.log("userInfos", this.userInfos);
+        // const grouped =this.groupBy(this.userInfos, pet => pet.userUniCode);
 
-      // });     
-    //   this.userInfosAfter.forEach(item=>{
-    //     this.userInfoAfterGroup.push(item);
-     // console.log('userInfosAfter',this.userInfosAfter);
-    //  });
+        // grouped.forEach(item=>{
+        // this.userInfosAfter.push(item[0]);
+
+        // });
+        //   this.userInfosAfter.forEach(item=>{
+        //     this.userInfoAfterGroup.push(item);
+        // console.log('userInfosAfter',this.userInfosAfter);
+        //  });
       });
-
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-   
-  }
+  //  groupBy(list, keyGetter) {
+  //           const map = new Map();
+  //           list.forEach((item) => {
+  //               const key = keyGetter(item);
+  //               const collection = map.get(key);
+  //               if (!collection) {
+  //                   map.set(key, [item]);
+  //               } else {
+  //                   collection.push(item);
+  //               }
+  //           });
+  //           return map;
+  //       }
 
-
-
-
-//  groupBy(list, keyGetter) {
-//           const map = new Map();
-//           list.forEach((item) => {
-//               const key = keyGetter(item);
-//               const collection = map.get(key);
-//               if (!collection) {
-//                   map.set(key, [item]);
-//               } else {
-//                   collection.push(item);
-//               }
-//           });
-//           return map;
-//       }
-
-  getRowClass(row){
-
-  }
-  async open(row){
+  getRowClass(row) {}
+  async open(row) {
     console.log(row);
-    this.router.navigate(['/user-payment-details',row.key])
+    this.router.navigate(["/user-payment-details", row.key]);
   }
 }
